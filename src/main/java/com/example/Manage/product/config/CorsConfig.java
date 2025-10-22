f5c18d69d47fceeb8448b1155e030d3f1fd19bf3
rincedonfack5 <rincedonfack5@gmail.com>
2025-10-22 12:55:47 +0100

    config

--- src/main/java/com/example/Manage/product/config/CorsConfig.java
+++ src/main/java/com/example/Manage/product/config/CorsConfig.java
@@ -1,4 +1,27 @@
 package com.example.Manage.product.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173")); // Vite et CRA
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
